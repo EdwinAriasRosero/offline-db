69ea4c2ebbb028e7e2715d8974167c6d9765b144
Edwin Arias <earias10@slb.com>
2024-09-16 15:07:11 -0500

    add deep object for replacing dates

--- src/server/core/jsonUtilities.ts
+++ src/server/core/jsonUtilities.ts
@@ -1,13 +1,33 @@
-export function replacer(key: string, value: any) {
+export function replacer(key: string, value: any): any {
     if (value instanceof Date) {
         return { __type: 'Date', value: value.toISOString() };
+    }
+    if (value && typeof value === 'object') {
+        if (Array.isArray(value)) {
+            return value.map(item => replacer(key, item));
+        }
+        for (const k in value) {
+            if (value.hasOwnProperty(k)) {
+                value[k] = replacer(k, value[k]);
+            }
+        }
     }
     return value;
 }
 
-export function reviver(key: string, value: any) {
+export function reviver(key: string, value: any): any {
     if (value && value.__type === 'Date') {
         return new Date(value.value);
+    }
+    if (value && typeof value === 'object') {
+        if (Array.isArray(value)) {
+            return value.map(item => reviver(key, item));
+        }
+        for (const k in value) {
+            if (value.hasOwnProperty(k)) {
+                value[k] = reviver(k, value[k]);
+            }
+        }
     }
     return value;
 }
